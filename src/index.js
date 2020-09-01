79065f1a981fd2803b50af9299f53235a852ef33
VitorManoel0 <49772585+VitorManoel0@users.noreply.github.com>
2020-09-01 16:37:34 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+
+const { uuid, isUuid } = require('uuidv4');
+
+const { request } = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const projects = [];
+
+function logRequests (request, response, next){
+  const { method,url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`
+
+  console.time(logLabel);
+
+  next(); //Próximo middleware
+
+  console.timeEnd(logLabel)
+}
+
+function validadeProjectId (request, response, next){
+  const {id} = request.params;
+
+  if (!isUuid(id)){
+    return response.status(400).json({error: 'Invalidad Project ID'});
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validadeProjectId);
+
+app.get('/projects', (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
+});
+
+app.post('/projects', (request, response) => {
+  const {title, owner} = request.body;
+
+  const project = { id: uuid(),title,owner};
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+app.put('/projects/:id', (request, response) => {
+
+  const {id} = request.params;
+  const {title, owner} = request.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0){
+    return response.status(404).json({ error: 'Project not found.'})
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0){
+    return response.status(404).json({ error: 'Project not found.'})
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.listen(3333, ()=> {
+  console.log('🥰🥰')
+});
